d6ff59dc1abf98c4f85f828097a079d0d91f62bf
Khanspii <1940930@brunel.ac.uk>
2021-11-10 20:18:23 +0000

    Added Booleans.js and StringAndNumbers.js

---
+++ Variables and Types/Booleans.js
@@ -0,0 +1,16 @@
+var booleanTrue = true;
+var booleanFalse = false;
+
+var string1 = "my name", string2 = "my surname";
+
+// if(booleanFalse == booleanFalse){
+//     console.log("Yes");
+// }else{
+//     console.log("No");
+// }
+
+if(string1 !== string1){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
---
+++ Variables and Types/StringAndNumbers.js
@@ -0,0 +1,26 @@
+var myString = "This is my String. Leave it alone";
+
+var len = myString.length;
+// len = 33;
+
+console.log(myString.toUpperCase());
+//THIS IS MY STRING. LEAVE IT ALONE
+
+//Numbers
+console.log(Infinity);
+
+var myNum = 12.3334;
+myNum = Math.round(myNum);
+console.log(myNum);
+
+var random = Math.random();
+console.log(random);
+
+var diceRandom = Math.round(Math.random() * 6); 
+//Random number between 0,1 muptiplyed by 6 and rounded
+console.log(diceRandom);
+console.log(Math.round(Math.random() * 6));
+console.log(Math.round(Math.random() * 6));
+console.log(Math.round(Math.random() * 6));
+console.log(Math.round(Math.random() * 6));
+console.log(Math.round(Math.random() * 6));
